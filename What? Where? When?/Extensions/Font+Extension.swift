6de804b6bb90d8bb55f463e409a460b2b4d98fa1
AnriBeridzee <aberidze02@gmail.com>
2024-01-31 19:23:57 +0400

    Add Custom Fonts

---
+++ What? Where? When?/Extensions/Font+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Font+Extension.swift
+//  What? Where? When?
+//
+//  Created by Macbook Air 13 on 31.01.24.
+//
+
+import UIKit
+
+extension UIFont {
+    public static let ninoMkhedruli = UIFont(name: "BPGNinoMkhedruli", size: 16.0)
+    public static let ninoMkhedruliBold = UIFont(name: "BPGNinoMkhedruli-Bold", size: 16.0)
+    public static let ninoMkhedruliBook = UIFont(name: "BPGNinoMkhedruliBook", size: 16.0)
+    public static let ninoMtavruli = UIFont(name: "BPGNinoMtavruli", size: 16.0)
+    public static let ninoMtavruliBold = UIFont(name: "BPGNinoMtavruli-Bold", size: 16.0)
+}
