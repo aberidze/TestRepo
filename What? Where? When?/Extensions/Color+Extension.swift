b43d2448a641a30c7d26fab9262386d1be05762a
AnriBeridzee <aberidze02@gmail.com>
2024-01-31 17:46:10 +0400

    Add More Custom Colors to Extension

--- What? Where? When?/Extensions/Color+Extension.swift
+++ What? Where? When?/Extensions/Color+Extension.swift
@@ -10,5 +10,14 @@ import UIKit
 extension UIColor {
     public static let backgroundGray = UIColor(red: 30 / 255.0, green: 31 / 255.0, blue: 33 / 255.0, alpha: 1.0)
     public static let backgroundGreen = UIColor(red: 38 / 255.0, green: 114 / 255.0, blue: 55 / 255.0, alpha: 1.0)
-    public static let backgroundRed = UIColor(red: 236 / 255.0, green: 28  / 255.0, blue: 36 / 255.0, alpha: 1.0)
+    public static let backgroundRed = UIColor(red: 236 / 255.0, green: 28 / 255.0, blue: 36 / 255.0, alpha: 1.0)
+    public static let backgroundDarkGold = UIColor(red: 145 / 255.0, green: 104 / 255.0, blue: 58 / 255.0, alpha: 1.0)
+    public static let backgroundGold = UIColor(red: 216 / 255.0, green: 163 / 255.0, blue: 109 / 255.0, alpha: 1.0)
+    public static let backgroundDarkBlue = UIColor(red: 31 / 255.0, green: 37 / 255.0, blue: 50 / 255.0, alpha: 1.0)
+    
+    public static let accentBlue = UIColor(red: 30 / 255.0, green: 37 / 255.0, blue: 124 / 255.0, alpha: 1.0)
+    public static let accentYellow = UIColor(red: 250 / 255.0, green: 181 / 255.0, blue: 31 / 255.0, alpha: 1.0)
+    public static let accentGold = UIColor(red: 148 / 255.0, green: 131 / 255.0, blue: 87 / 255.0, alpha: 1.0)
+    public static let accentLightGray = UIColor(red: 197 / 255.0, green: 197 / 255.0, blue: 197 / 255.0, alpha: 1.0)
+    public static let accentDarkGray = UIColor(red: 151 / 255.0, green: 151 / 255.0, blue: 151 / 255.0, alpha: 1.0)
 }
