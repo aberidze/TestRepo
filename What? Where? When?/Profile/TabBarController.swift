75fcf7782426af51c2e979885cef12d82b64ee67
AnriBeridzee <aberidze02@gmail.com>
2024-02-02 06:16:32 +0400

    Add tab bar controller

---
+++ What? Where? When?/Profile/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  What? Where? When?
+//
+//  Created by Macbook Air 13 on 02.02.24.
+//
+
+import UIKit
+
+final class TabBarController: UIViewController {
+    
+    // MARK: - View Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
